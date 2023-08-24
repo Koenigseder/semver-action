7305dc8c2e1bfa1c56dc2459851706f385ad5627
Koenigseder <kevin.koenigseder@outlook.com>
2023-08-24 18:10:14 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+
+const githubToken: string = core.getInput("github-token", { required: true });
+const baseBranch: string = core.getInput("base-branch");
+const majorReleaseTag: string = core.getInput("major-release-tag");
+const minorReleaseTag: string = core.getInput("minor-release-tag");
+const patchReleaseTag: string = core.getInput("patch-release-tag");
+
+const octokit = github.getOctokit(githubToken);
+const context = github.context;
+
+async function main() {
+  const pullRequestLabels = await octokit.rest.issues.listLabelsOnIssue({
+    issue_number: context.issue.number,
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+  });
+
+  console.log(pullRequestLabels);
+}
+
+main();
