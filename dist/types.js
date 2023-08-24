744cbd8288b4079d78fffec9f0fc01faec1b9555
Koenigseder <Koenigseder@users.noreply.github.com>
2023-08-24 17:09:23 +0000

    Update dist

---
+++ dist/types.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ReleaseType = void 0;
+var ReleaseType;
+(function (ReleaseType) {
+    ReleaseType["Major"] = "major";
+    ReleaseType["Minor"] = "minor";
+    ReleaseType["Patch"] = "patch";
+})(ReleaseType || (exports.ReleaseType = ReleaseType = {}));
